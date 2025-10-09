710ed30b3151c8fe05241676e1d7645f00478143
vikasdongrepawar <vikasdongre952@gmail.com>
2025-10-09 09:08:03 +0530

    feat: add cookie-parser middleware and implement user registration logic

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require('express')
 
+const authRoutes = require('./models/routes/auth.routes')
 
-const app = express();
+const cookieParser = require('cookie-parser')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+app.use(cookieParser())
+
+// Use auth routes
+
+app.use('/api/auth', authRoutes)
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Social Media API');
-});
+  res.send('Welcome to the Social Media API')
+})
 
 // More routes can be added here
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,22 @@
+const userModel = require('../modeles/user.model')
+
+const jwt = require('jsonwebtoken')
+
+
+
+
 async function registerController(req, res) {
   const { username, password } = req.body
-}
 
-async function loginController(req, res) {
-  const { username, password } = req.body
+  const isUserAlreadyExists = await userModel.findOne({ username })
+
+  if (isUserAlreadyExists) {
+    return res.status(400).json({ message: 'User already exists' })
+  }
+
+  const newUser = new userModel({ username, password })
+
+  res.status(201).json({ message: 'User registered successfully', user: newUser })
 }
 
 async function loginController(req, res) {
